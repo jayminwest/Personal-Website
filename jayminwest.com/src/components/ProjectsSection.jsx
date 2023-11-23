0617f1187d79d7add80cc0a3b3afd378fd7fcae1
jaymin <jayminwest@gmail.com>
2023-11-23 11:35:45 -0700

    added descriptions and turned buttons into links in projects section

--- jayminwest.com/src/components/ProjectsSection.jsx
+++ jayminwest.com/src/components/ProjectsSection.jsx
@@ -19,29 +19,38 @@ export default function ProjectsSection() {
 // Sample data for the cards
 const cardData = [
     {
-        imageSrc: '..\..\public\img\snow-pack-proj-pic.PNG',
+        imageSrc: 'snow-pack-proj-pic.PNG',
         text: 'Snow Pack Prediction Using GPT-4',
+        description:"Custom trained GPT model using data gathered from avalanche forecast centers. Uses vector database and GPT to give user advice on dangerous snow conditions.",
+        link:"",
     },
     {
-        imageSrc: 'url-to-image-2.jpg',
+        imageSrc: 'jayminwest.com\public\img\ncaa-predictions-proj-pic.PNG',
         text: 'March Madness Prediction',
+        description:"Custom Neural Network trained on data from Kenpom to predict the outcome of NCAA Men's Basketball games",
+        link:"",
     },
     {
-        imageSrc: 'url-to-image-3.jpg',
-        text: 'https://github.com/jayminwest/Python-Sorting-Algorithms-Visualization',
+        imageSrc: 'jayminwest.com\public\img\sorting-algorithms-proj-pic.PNG',
+        text: "Sorting Algorithm Visualization",
+        description: "A simple program that displays common sorting algorithms using Tkinter",
+        link:'https://github.com/jayminwest/Python-Sorting-Algorithms-Visualization',
     },
     {
-        imageSrc: 'url-to-image-4.jpg',
+        imageSrc: 'jayminwest.com/public/img/forexgpt-proj-pic.png',
         text: 'Forex-GPT',
+        description:"A work in progess that aims to function as a real-time collection of GPT agents working to provide analysis on Forex markets",
+        link: "temp-link.com"
     },
 ];
 
 // Component for each card
-const Card = ({ imageSrc, text }) => (
+const Card = ({ imageSrc, text, description, link }) => (
     <Box borderWidth="1.5px" borderRadius="lg" overflow="hidden" p="4">
         <Image src={imageSrc} alt="Card Image" mb="4" />
-        <Text>{text}</Text>
-        <Button mt="4" colorScheme="red">
+        <Text fontWeight="bold">{text}</Text>
+        <Text>{description}</Text>
+        <Button as="a" mt="4" colorScheme="red" target="_blank" href={link}>
         Learn More
         </Button>
     </Box>
