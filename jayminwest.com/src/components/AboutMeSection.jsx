1389eb1318608a92cc9c1091cb2a35f4146a8303
jaymin <jayminwest@gmail.com>
2023-11-24 14:42:35 -0700

    added all headers and made component files

---
+++ jayminwest.com/src/components/AboutMeSection.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AboutMeSection() {
+  return (
+    <div>AboutMeSection</div>
+  )
+}
--- jayminwest.com/src/layouts/RootLayout.jsx
+++ jayminwest.com/src/layouts/RootLayout.jsx
@@ -2,6 +2,10 @@ import { Outlet } from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Header from "../components/Header"
 import ProjectsSection from "../components/ProjectsSection"
+import SkillsSection from "../components/SkillsSection"
+import LeadershipSection from "../components/LeadershipSection"
+import AboutMeSection from "../components/AboutMeSection"
+import ContactSection from "../components/ContactSection"
 import { Box, Spacer, Heading } from '@chakra-ui/react'
 
 export default function RootLayout() {
@@ -19,8 +23,16 @@ export default function RootLayout() {
       <Navbar />
       <Header />
       <Spacer />
-      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>Personal Projects</Heading>
+      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>PERSONAL PROJECTS</Heading>
       <ProjectsSection />
+      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>TECHNICAL SKILLS</Heading>
+      <SkillsSection />
+      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>LEADERSHIP EXPERIENCE</Heading>
+      <LeadershipSection />
+      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>ABOUT ME</Heading>
+      <AboutMeSection />
+      <Heading minW={"200px"} paddingBottom={10} paddingTop={40}>GET IN TOUCH</Heading>
+      <ContactSection />
       <Outlet />
     </Box>
   )
