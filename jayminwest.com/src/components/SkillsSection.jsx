8bdc6ee4f2ef70cd470e7404574c16e0493c4879
jaymin <jayminwest@gmail.com>
2023-11-25 09:31:14 -0700

    added svgs to technical skills section

--- jayminwest.com/src/components/SkillsSection.jsx
+++ jayminwest.com/src/components/SkillsSection.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import { ChakraProvider, Box, Grid } from "@chakra-ui/react";
 
-export default function SkillsSection() {
+const defaultSvgData = [
+  '../docker-logo.svg',
+  '../github-logo.svg',
+  '../jupyter-logo.svg',
+  '../python-logo.svg',
+  '../react-logo.svg',
+  '../c++-logo.svg',
+];
+
+const SkillsSection = ({ svgData = defaultSvgData }) => {
   return (
-    <div>SkillsSection</div>
-  )
-}
+    <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+      {svgData.map((svg, index) => (
+        <Box key={index} p={4} borderWidth="0px" borderRadius="lg">
+          <img src={svg} alt={`Logo ${index}`} />
+        </Box>
+      ))}
+    </Grid>
+  );
+};
+
+export default SkillsSection;
